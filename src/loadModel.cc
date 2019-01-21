f787adb4f6201c7ea8e850e758e7a18e0167d53f
Sylvain Perron <slvn.perron+github@gmail.com>
2019-01-21 14:48:57 -0500

    better error handling on loadModel

--- src/loadModel.cc
+++ src/loadModel.cc
@@ -1,29 +1,39 @@
 #include "loadModel.h"
 #include "node-argument.h"
 
-
-void LoadModel::Execute () {
-	try {
-        result_ = wrapper_->loadModel( filename );
-    } catch (std::string errorMessage) {
+void LoadModel::Execute()
+{
+    try
+    {
+        result_ = wrapper_->loadModel(filename);
+    }
+    catch (const char *str)
+    {
+        std::cout << "Exception: " << str << std::endl;
+        SetErrorMessage(str);
+    }
+    catch (std::string errorMessage)
+    {
         SetErrorMessage(errorMessage.c_str());
     }
 }
 
-void LoadModel::HandleErrorCallback () {
+void LoadModel::HandleErrorCallback()
+{
     Nan::HandleScope scope;
     auto res = GetFromPersistent("key").As<v8::Promise::Resolver>();
-    res->Reject( Nan::GetCurrentContext() , Nan::Error(ErrorMessage()));
+    res->Reject(Nan::GetCurrentContext(), Nan::Error(ErrorMessage()));
     v8::Isolate::GetCurrent()->RunMicrotasks();
 }
 
-void LoadModel::HandleOKCallback () {
-	
+void LoadModel::HandleOKCallback()
+{
+
     Nan::HandleScope scope;
     NodeArgument::NodeArgument nodeArg;
-    v8::Local<v8::Object> result = nodeArg.mapToObject( result_ );
+    v8::Local<v8::Object> result = nodeArg.mapToObject(result_);
 
     auto res = GetFromPersistent("key").As<v8::Promise::Resolver>();
-    res->Resolve( Nan::GetCurrentContext() , result);
+    res->Resolve(Nan::GetCurrentContext(), result);
     v8::Isolate::GetCurrent()->RunMicrotasks();
 }
